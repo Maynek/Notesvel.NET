78fe11a7212f03997c373144fdfa20fe442e1301
Ada Maynek <ada.maynek@gmail.com>
2024-08-28 16:40:41 +0900

    no message

--- Notesvel/src/Console/Program.cs
+++ Notesvel/src/Console/Program.cs
@@ -1,7 +1,8 @@
-﻿//********************************
+//********************************
 // (c) 2024 Ada Maynek
 // This software is released under the MIT License.
 //********************************
+using System;
 using Maynek.Notesvel.Reader;
 using Maynek.Notesvel.Writer.MySite;
 using Maynek.Notesvel.Writer.Narou;
@@ -12,8 +13,9 @@ namespace Maynek.Notesvel.Console
     {
         class Parameter
         {
-            public string InputDirectory { get; private set; } = string.Empty;
-            public string OutputDirectory { get; private set; } = string.Empty;
+            public string ProjectName { get; private set; } = string.Empty;
+            public string InputRoot { get; private set; } = string.Empty;
+            public string OutputRoot { get; private set; } = string.Empty;
             
             public static Parameter CreateParameter(string[] args)
             {
@@ -21,12 +23,21 @@ namespace Maynek.Notesvel.Console
 
                 var parser = new Parser();
 
+                parser.AddOptionDefinition(new OptionDefinition("-p", "--project")
+                {
+                    Type = OptionType.RequireValue,
+                    EventHandler = delegate (object sender, OptionEventArgs e)
+                    {
+                        parameter.ProjectName = e.Value;
+                    }
+                });
+
                 parser.AddOptionDefinition(new OptionDefinition("-i", "--input")
                 {
                     Type = OptionType.RequireValue,
                     EventHandler = delegate (object sender, OptionEventArgs e)
                     {
-                        parameter.InputDirectory = e.Value;
+                        parameter.InputRoot = e.Value;
                     }
                 });
 
@@ -35,7 +46,7 @@ namespace Maynek.Notesvel.Console
                     Type = OptionType.RequireValue,
                     EventHandler = delegate (object sender, OptionEventArgs e)
                     {
-                        parameter.OutputDirectory = e.Value;
+                        parameter.OutputRoot = e.Value;
                     }
                 });
 
@@ -48,35 +59,54 @@ namespace Maynek.Notesvel.Console
             static void Main(string[] args)
             {
                 var param = Parameter.CreateParameter(args);
-                if (param.InputDirectory != string.Empty && param.OutputDirectory != string.Empty)
+
+                if (param.ProjectName == string.Empty)
                 {
-                    string inputEpisodeDirectory = Path.Combine(param.InputDirectory, @"episodes\");
-                    string inputNoteDirectory = Path.Combine(param.InputDirectory, @"notes\");
-
-                    //Read index.xml
-                    var novel = NovelReader.Read(Path.Combine(param.InputDirectory, "index.xml"));
-
-                    //Setup Novel
-                    novel.SetEpisodePagenation();
-
-
-                    //Write for My Web Site.
-                    new MySiteWriter()
-                    {
-                        InputEpisodeDirectory = inputEpisodeDirectory,
-                        InputNoteDirectory = inputNoteDirectory,
-                        OutputEpisodeDirectory = Path.Combine(param.OutputDirectory, @"site\"),
-                        OutputNoteDirectory = Path.Combine(param.OutputDirectory, @"site\note\")
-                    }.Write(novel);
-
-                    //Write for Narou.
-                    new NarouWriter()
-                    {
-                        InputEpisodeDirectory = inputEpisodeDirectory,
-                        OutputEpisodeDirectory = Path.Combine(param.OutputDirectory, @"narou\"),
-                    }.Write(novel);
-
+                    System.Console.WriteLine("Project Name is not set.");
+                    return;
                 }
+
+                if (param.InputRoot == string.Empty)
+                {
+                    System.Console.WriteLine("Input Directory is not set.");
+                    return;
+                }
+
+                if (param.OutputRoot == string.Empty)
+                {
+                    System.Console.WriteLine("Output Directory is not set.");
+                    return;
+                }
+
+                string inputDirectory = Path.Combine(param.InputRoot, param.ProjectName);
+                string inputEpisodeDirectory = Path.Combine(inputDirectory, @"episodes\");
+                string inputNoteDirectory = Path.Combine(inputDirectory, @"notes\");
+                string inputIndexPath = Path.Combine(inputDirectory, "index.xml");
+
+                //Read index.xml
+                var novel = NovelReader.Read(inputIndexPath);
+
+                //Setup Novel
+                novel.SetEpisodePagenation();
+
+                //Write for My Web Site.
+                string siteEpisodeDirectory = Path.Combine(param.OutputRoot, @"site\", param.ProjectName);
+                string siteNoteDirectory = Path.Combine(siteEpisodeDirectory, @"note\");
+                new MySiteWriter()
+                {
+                    InputEpisodeDirectory = inputEpisodeDirectory,
+                    InputNoteDirectory = inputNoteDirectory,
+                    OutputEpisodeDirectory = siteEpisodeDirectory,
+                    OutputNoteDirectory = siteNoteDirectory
+                }.Write(novel);
+
+                //Write for Narou.
+                string narouEpisodeDirectory = Path.Combine(param.OutputRoot, @"narou\", param.ProjectName);
+                new NarouWriter()
+                {
+                    InputEpisodeDirectory = inputEpisodeDirectory,
+                    OutputEpisodeDirectory = narouEpisodeDirectory,
+                }.Write(novel);                
             }
         }
     }
